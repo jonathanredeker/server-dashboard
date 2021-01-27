43ef438caea4b60ad4c400666df83445be79040d
Jonathan Redeker <jonathanredeker@live.com>
2021-01-27 13:13:39 -0800

    Added a nice design to display nodes

--- src/Views/NodeDashboard.js
+++ src/Views/NodeDashboard.js
@@ -1,6 +1,5 @@
 import react from 'react'
-import { Container, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import NodeItem from '../Components/NodeItem'
 import { useAxiosGet } from '../Hooks/HttpRequest'
 
@@ -10,25 +9,19 @@ function NodeDashboard() {
     const endpointURL = `https://600f10ec6c21e1001704e67a.mockapi.io/api/v1/stats?p=${page}&l=${limit}`
     let nodes = useAxiosGet(endpointURL)
     let content = null
-
+    
     if (nodes.data) {
-        content = 
-            <div className="">
-                <div className="">
-                    {
-                        nodes.data.map((node, key) => 
-                            <NodeItem 
-                                data={node}
-                                key={key}
-                            />
-                        )
-                    }
-                </div>
-            </div>
+        content = nodes.data.map((node, key) => 
+            <NodeItem 
+                data={node}
+                key={key}
+            />
+        )
+            
     }
 
     return (
-        <div class="node-list-wrapper">
+        <div class="row m-0 pt-4 d-flex justify-content-around">
             {content}
         </div>
     )
