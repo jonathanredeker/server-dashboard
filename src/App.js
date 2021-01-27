0f61b003bbd31b22c18c9065c50155175d4003e2
Jonathan Redeker <jonathanredeker@live.com>
2021-01-27 13:17:36 -0800

    Fixed an issue that wouldn't allow routing to the NodeDetail view

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './Components/Header'
+import Navigation from './Components/Navigation'
 import NodeDashboard from './Views/NodeDashboard'
 import NodeDetails from './Views/NodeDetails'
 
@@ -6,31 +9,28 @@ function App() {
 
     return (
 
-        <div>
+        <BrowserRouter>
 
-            <BrowserRouter>
-
+            <div className="container-fluid p-0 pb-5">
+                <Navigation />
                 <Switch>
                     <Route exact path="/">
-                        <NodeDashboard />
-                    </Route>
-                    <Route exact path="/:page">
-                        <NodeDashboard />
-                    </Route>
-                    <Route path="/:page/:limit">
-                        <NodeDashboard />
-                    </Route>
-                    <Route exact path="/node">
                         <NodeDashboard />
                     </Route>
                     <Route exact path="/node/:id">
                         <NodeDetails />
                     </Route>
+                    <Route exact path="/:page">
+                        <NodeDashboard />
+                    </Route>
+                    <Route exact path="/:page/:limit">
+                        <NodeDashboard />
+                    </Route>
                 </Switch>
+            </div>
 
-            </BrowserRouter>
 
-        </div>
+        </BrowserRouter>
 
     )
     
