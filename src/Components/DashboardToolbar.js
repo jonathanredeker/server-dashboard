4abfc6aec89f47788e70062c258d3bbc65ce8642
Jonathan Redeker <jonathanredeker@live.com>
2021-01-28 14:37:47 -0800

    Formatted for readability

--- src/Components/DashboardToolbar.js
+++ src/Components/DashboardToolbar.js
@@ -6,17 +6,38 @@ function DashboardToolbar(props) {
 
     // Dropdown pagination
     if (props.page > 1) {
-        dropdowns.push(<Dropdown.Item href={`/${props.page - 1}/${props.limit}`}>Page {props.page - 1}</Dropdown.Item>)
+        dropdowns.push(
+            <Dropdown.Item 
+                href={`/${props.page - 1}/${props.limit}`}
+            >
+                Page {props.page - 1}
+            </Dropdown.Item>)
     }
-    dropdowns.push(<Dropdown.Item href={`/${props.page}/${props.limit}`}>Page {props.page}</Dropdown.Item>)
+    
+    dropdowns.push(
+        <Dropdown.Item 
+            href={`/${props.page}/${props.limit}`}
+        >
+            Page {props.page}
+        </Dropdown.Item>)
+
     if (props.page < props.totalNodes / props.limit) {
-        dropdowns.push(<Dropdown.Item href={`/${props.page + 1}/${props.limit}`}>Page {props.page + 1}</Dropdown.Item>)
+        dropdowns.push(
+            <Dropdown.Item 
+                href={`/${props.page + 1}/${props.limit}`}
+            >
+                Page {props.page + 1}
+            </Dropdown.Item>)
     }
 
     return (
-        <div className="row ml-4 mr-4 pl-3 pr-3 mt-5 d-flex justify-content-end">
-            <div className="col-sm-auto mt-2 d-flex justify-content-end">
-                <DropdownButton id="dropdown-basic-button" title={`Page ${props.page}`}>
+        <div className="row ml-4 mr-4 pl-3 pr-3 mt-5 d-flex justify-content-end flex-nowrap">
+            <div className="col-sm-auto mt-2 p-0 d-flex justify-content-end">
+                <DropdownButton 
+                    id="dropdown-basic-button" 
+                    title={`Page ${props.page}`}
+                    size="sm"
+                >
                     <Dropdown.Item href={`/1/${props.limit}`}>First Page</Dropdown.Item>
                     <Dropdown.Divider />
                     {dropdowns}
@@ -24,8 +45,12 @@ function DashboardToolbar(props) {
                     <Dropdown.Item href={`/${props.totalNodes / props.limit}/${props.limit}`}>Last Page</Dropdown.Item>
                 </DropdownButton>
             </div>
-            <div className="col-sm-auto mt-2">
-                <DropdownButton id="dropdown-basic-button" title={`Limit Results (${props.limit})`}>
+            <div className="col-sm-auto mt-2 ml-3 p-0">
+                <DropdownButton 
+                    id="dropdown-basic-button" 
+                    title={`Limit Results (${props.limit})`}
+                    size="sm"
+                >
                     <Dropdown.Item href={`/1/10`}>10 Results</Dropdown.Item>
                     <Dropdown.Item href={`/1/20`}>20 Results</Dropdown.Item>
                     <Dropdown.Item href={`/1/30`}>30 Results</Dropdown.Item>
