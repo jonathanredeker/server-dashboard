6fc4519fe01c5caa636daf5e42569dce11ff0f61
Jonathan Redeker <jonathanredeker@live.com>
2021-01-28 12:01:49 -0800

    Changed formatting

--- src/Hooks/HttpRequest.js
+++ src/Hooks/HttpRequest.js
@@ -9,58 +9,56 @@ export function useAxiosGet(endpointURL) {
         error: false,
     })
 
-    useEffect(
-        () => {
-            setRequest({
-                loading: true,
-                data: null,
-                error: false,
+    useEffect(() => {
+        // Replace this
+        setRequest({
+            loading: true,
+            data: null,
+            error: false,
+        })
+        axios.get(endpointURL)
+            .then(response => {
+                setRequest({
+                    loading: false,
+                    data: response.data,
+                    error: false,
+                })
             })
-            axios.get(endpointURL)
-                .then(response => {
-                    setRequest({
-                        loading: false,
-                        data: response.data,
-                        error: false,
-                    })
+            .catch(() => {
+                setRequest({
+                    loading: false,
+                    data: null,
+                    error: true,
                 })
-                .catch(() => {
-                    setRequest({
-                        loading: false,
-                        data: null,
-                        error: true,
-                    })
-                })
+            })
         },
         [endpointURL]
     )
 
-    useInterval(
-        () => {
-            setRequest({
-                loading: true,
-                data: null,
-                error: false,
+    useInterval(() => {
+        setRequest({
+            loading: true,
+            data: null,
+            error: false,
+        })
+        axios.get(endpointURL)
+            .then(response => {
+                setRequest({
+                    loading: false,
+                    data: response.data,
+                    error: false,
+                })
             })
-            axios.get(endpointURL)
-                .then(response => {
-                    setRequest({
-                        loading: false,
-                        data: response.data,
-                        error: false,
-                    })
+            .catch(() => {
+                setRequest({
+                    loading: false,
+                    data: null,
+                    error: true,
                 })
-                .catch(() => {
-                    setRequest({
-                        loading: false,
-                        data: null,
-                        error: true,
-                    })
-                })
+            })
         },
         300000
     )
-
     return request
 }
 
@@ -68,20 +66,18 @@ export function useAxiosGetTotalNodes() {
     const endpointURL = `https://600f10ec6c21e1001704e67a.mockapi.io/api/v1/stats`
     const [nodeCount, setNodeCount] = useState(0)
 
-    useEffect(
-        () => {
-            axios.get(endpointURL)
-                .then(response => {
-                    if (response.data) {
-                        setNodeCount(response.data.length)
-                    }
-                })
-                .catch(() => {
-                    setNodeCount(0)
-                })
+    useEffect(() => {
+        axios.get(endpointURL)
+            .then(response => {
+                if (response.data) {
+                    setNodeCount(response.data.length)
+                }
+            })
+            .catch(() => {
+                setNodeCount(0)
+            })
         },
         [endpointURL]
     )
-
     return nodeCount
 }
