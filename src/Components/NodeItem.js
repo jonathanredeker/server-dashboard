7e9ba3bc46c772987e047df6b7824c06f970abea
Jonathan Redeker <jonathanredeker@live.com>
2021-01-28 13:56:32 -0800

    Clean up code and remove redundancies

--- src/Components/NodeItem.js
+++ src/Components/NodeItem.js
@@ -1,37 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Meter from '../Components/Meter'
+const date = require('date-and-time');
 
 function NodeItem(props) {
-
-    // RAM statistics
-    const allocatedRAM = props.data.allocated_ram
-    const freeRAM = props.data.free_ram
-    const totalRAM = freeRAM + allocatedRAM
-    const ramUsage = Math.trunc(allocatedRAM / totalRAM * 100)
-
-    // Disk statistics
-    const allocatedDisk = props.data.allocated_disk
-    const freeDisk = props.data.free_disk
-    const totalDisk = freeRAM + allocatedDisk
-    const diskUsage = Math.trunc(allocatedDisk / totalDisk * 100)
-
-    // Date since node went up
+    const totalRAM = props.data.free_ram + props.data.allocated_ram
+    const totalDisk = props.data.free_disk + props.data.allocated_disk
+    const ramUsage = Math.trunc(props.data.allocated_ram / totalRAM * 100)
+    const diskUsage = Math.trunc(props.data.allocated_disk / totalDisk * 100)
     const upSince = new Date(props.data.up_since)
-    const monthNames = [
-        "January", 
-        "February", 
-        "March", 
-        "April", 
-        "May", 
-        "June", 
-        "July", 
-        "August", 
-        "September", 
-        "October", 
-        "November", 
-        "December"
-    ]
 
     return (
         <div className="node-item ml-4 mr-4 col-lg-5 rounded p-4 mt-4" key={props.data.key}>
@@ -41,7 +18,7 @@ function NodeItem(props) {
                         Node {props.data.id}
                     </h5>
                     <small className="text-muted p-0 font-weight-light">
-                        Active since {monthNames[upSince.getMonth()]} {upSince.getDay()+1}, {upSince.getFullYear()}
+                        Active since {date.format(upSince, "MMMM D, YYYY")}
                     </small>
                 </div>
                 <div className="col-sm-6 col-6 d-flex justify-content-end">
@@ -52,10 +29,12 @@ function NodeItem(props) {
             </div>
             <Meter
                 label="RAM"
+                labelValue={`${ramUsage}%`}
                 value={ramUsage}
             />
             <Meter
                 label="Disk"
+                labelValue={`${diskUsage}%`}
                 value={diskUsage}
             />
         </div>
