115184d75512cacfecaf6565aa22e2dc6dfad303
Jonathan Redeker <jonathanredeker@live.com>
2021-01-28 11:57:17 -0800

    Changed the way pagination behaves

--- src/Components/DashboardToolbar.js
+++ src/Components/DashboardToolbar.js
@@ -2,18 +2,34 @@ import React from 'react'
 import { DropdownButton, Dropdown } from 'react-bootstrap'
 
 function DashboardToolbar(props) {
+    let dropdowns = []
+
+    // Dropdown pagination
+    if (props.page > 1) {
+        dropdowns.push(<Dropdown.Item href={`/${props.page - 1}/${props.limit}`}>Page {props.page - 1}</Dropdown.Item>)
+    }
+    dropdowns.push(<Dropdown.Item href={`/${props.page}/${props.limit}`}>Page {props.page}</Dropdown.Item>)
+    if (props.page < props.totalNodes / props.limit) {
+        dropdowns.push(<Dropdown.Item href={`/${props.page + 1}/${props.limit}`}>Page {props.page + 1}</Dropdown.Item>)
+    }
+
     return (
-        <div class="row ml-5 mr-5 pl-3 pr-3 mt-5">
-            <div class="col-md-10 d-flex justify-content-end">
+        <div className="row ml-4 mr-4 pl-3 pr-3 mt-5 d-flex justify-content-end">
+            <div className="col-sm-auto mt-2 d-flex justify-content-end">
                 <DropdownButton id="dropdown-basic-button" title={`Page ${props.page}`}>
-                    {props.pageDropdowns}
+                    <Dropdown.Item href={`/1/${props.limit}`}>First Page</Dropdown.Item>
+                    <Dropdown.Divider />
+                    {dropdowns}
+                    <Dropdown.Divider />
+                    <Dropdown.Item href={`/${props.totalNodes / props.limit}/${props.limit}`}>Last Page</Dropdown.Item>
                 </DropdownButton>
             </div>
-            <div class="col-md-2 d-flex justify-content-end">
+            <div className="col-sm-auto mt-2">
                 <DropdownButton id="dropdown-basic-button" title={`Limit Results (${props.limit})`}>
                     <Dropdown.Item href={`/1/10`}>10 Results</Dropdown.Item>
                     <Dropdown.Item href={`/1/20`}>20 Results</Dropdown.Item>
                     <Dropdown.Item href={`/1/30`}>30 Results</Dropdown.Item>
+                    <Dropdown.Divider />
                     <Dropdown.Item href={`/1/${props.totalNodes}`}>All Results ({`${props.totalNodes}`})</Dropdown.Item>
                 </DropdownButton>
             </div>
