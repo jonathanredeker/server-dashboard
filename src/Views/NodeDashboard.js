c3914c455f643b25b00e197964c1c3e3b319470f
Jonathan Redeker <jonathanredeker@live.com>
2021-01-28 13:57:30 -0800

    Added Error component

--- src/Views/NodeDashboard.js
+++ src/Views/NodeDashboard.js
@@ -3,13 +3,14 @@ import { useParams } from 'react-router-dom'
 import NodeItem from '../Components/NodeItem'
 import { useAxiosGet, useAxiosGetTotalNodes } from '../Hooks/HttpRequest'
 import Loader from '../Components/Loader'
+import Error from '../Components/Error';
 import DashboardToolbar from '../Components/DashboardToolbar'
 
 function NodeDashboard() {
     const {page = 1, limit = 10} = useParams()
     const endpointURL = `https://600f10ec6c21e1001704e67a.mockapi.io/api/v1/stats?p=${page}&l=${limit}`
-    let nodes = useAxiosGet(endpointURL)
     let totalNodes = useAxiosGetTotalNodes()
+    let nodes = useAxiosGet(endpointURL)
     let toolbar = null
     let content = null
     
@@ -33,12 +34,12 @@ function NodeDashboard() {
                 />
             )
     }
-
+    
     if (nodes.error) {
         content = 
-            <div className="alert alert-danger m-4 mt-5 text-danger" role="alert">
-                Could not retrieve data from the endpoint; please contact a server administrator!
-            </div>
+            <Error
+                message="Could not retrieve data from the endpoint. Please contact a server administrator."
+            />
     }
 
     return (
