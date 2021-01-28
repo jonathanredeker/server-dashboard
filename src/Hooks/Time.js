397c823da26064d1343705f5f8b64267441b8a5d
Jonathan Redeker <jonathanredeker@live.com>
2021-01-27 17:37:38 -0800

    Clean-up code

--- src/Hooks/Time.js
+++ src/Hooks/Time.js
@@ -4,15 +4,21 @@ import React, { useEffect, useRef } from 'react'
 export function useInterval(callback, timeout) {
     const callbackRef = useRef()
 
-    useEffect(() => {
-        callbackRef.current = callback
-    }, [callback])
+    useEffect(
+        () => {
+            callbackRef.current = callback
+        }, 
+        [callback]
+    )
 
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            callbackRef.current()
-        }, timeout)
-        return () => clearInterval(intervalID)
-    }, [timeout])
+    useEffect(
+        () => {
+            const intervalID = setInterval(
+                () => callbackRef.current(), timeout
+            )
+            return () => clearInterval(intervalID) // Memory leaks aren't fun.
+        }, 
+        [timeout]
+    )
     
 }
