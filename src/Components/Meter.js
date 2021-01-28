6b1c5409ec1213941c7e1b1eecde97c54ba21e63
Jonathan Redeker <jonathanredeker@live.com>
2021-01-28 11:59:52 -0800

    A reusable meter

---
+++ src/Components/Meter.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar'
+
+function Meter(props) {
+    let variant;
+
+    if (props.value < 50) {
+        variant = "success"
+    } else if (props.value < 80) {
+        variant = "warning"
+    } else {
+        variant = "danger"
+    }
+
+    return (
+        <div>
+            <div className="row progress-labels pb-2 d-flex justify-content-between text-light font-weight-light">
+                <div className="col meter-label">
+                    {props.label}
+                </div>
+                <div className="col meter-value text-right">
+                    {props.value}%
+                </div>
+            </div>
+            <div className="row pb-3">
+                <div className="col">
+                    <ProgressBar animated variant={variant} now={props.value}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Meter
